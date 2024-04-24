2c272c845f6e79abf6e11204ae35e296755fd297
Kod Enigma <olamilekanadeleke123@gmail.com>
2024-04-24 10:33:17 +0100

    feat: fix evn config

--- lib/app/env.dart
+++ lib/app/env.dart
@@ -6,15 +6,4 @@ class ENV {
   String get apiBaseUrl => dotenv.env['STAGING_BASE_URL'] ?? "";
 
   String get prodApiBaseUrl => dotenv.env['PROD_BASE_URL'] ?? "";
-
-  String get DIGITAL_OCEAN_REGION => dotenv.env['DIGITAL_OCEAN_REGION'] ?? "";
-
-  String get digitalOceanAccessKey =>
-      dotenv.env['DIGITAL_OCEAN_PUBLIC_KEY'] ?? "";
-
-  String get digitalOceanSecretKey =>
-      dotenv.env['DIGITAL_OCEAN_SECRET_KEY'] ?? "";
-
-  String get digitalOceanBucketUrl =>
-      dotenv.env['DIGITAL_OCEAN_BASE_URL'] ?? "";
 }
--- lib/app/locator.dart
+++ lib/app/locator.dart
@@ -10,6 +10,7 @@ import '../cores/shared_features/image_upload/image_upload_locator.dart';
 import '../cores/shared_features/loading_overlay/bloc/loading_over_lay_bloc.dart';
 import '../cores/shared_session/authentication_local_storage_data_source.dart';
 import '../cores/utils/utils.dart';
+import '../features/auth/auth_locator.dart';
 import 'env.dart';
 
 final GetIt getIt = GetIt.instance;
@@ -29,7 +30,7 @@ class Locator {
     setUpImageUploadLocator();
 
     /// -------- Authentication -------- ///
-    // setUpAuthenticationLocators();
+    setUpAuthenticationLocators();
 
     /// ------------- Misc -------------//
     setUpBankMiscLocator();
